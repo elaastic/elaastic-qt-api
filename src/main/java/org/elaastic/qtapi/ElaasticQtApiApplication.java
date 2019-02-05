9dfd7cb2d48900612b882417a053fd4e78408cdd
Franck Silvestre <franck.silvestre@gmail.com>
2019-02-05 09:25:53 +0100

    Initial commit: spring boot project basic settings

---
+++ src/main/java/org/elaastic/qtapi/ElaasticQtApiApplication.java
@@ -0,0 +1,14 @@
+package org.elaastic.qtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElaasticQtApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElaasticQtApiApplication.class, args);
+    }
+
+}
+
