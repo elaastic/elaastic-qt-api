2677e6edd29bddef84d13c3eb10486b02a703f5f
Gromnur <flavienregis@gmail.com>
2019-02-13 17:39:52 +0100

    Add entity annotation

--- src/main/java/org/elaastic/qtapi/Entity/PeerGrading.java
+++ src/main/java/org/elaastic/qtapi/Entity/PeerGrading.java
@@ -1,11 +1,13 @@
 package org.elaastic.qtapi.Entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+@Entity
 public class PeerGrading {
 
     @Id
