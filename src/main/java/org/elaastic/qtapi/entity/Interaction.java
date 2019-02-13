0a75be75298529530f40fcb3f2a4fee8fe09e9e6
Gromnur <flavienregis@gmail.com>
2019-02-13 16:15:38 +0100

    Wait for RDV with the prof to continue

--- src/main/java/org/elaastic/qtapi/entity/Interaction.java
+++ src/main/java/org/elaastic/qtapi/entity/Interaction.java
@@ -5,122 +5,335 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.Map;
 
-public class Interaction {
+class Interaction {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    public static final String EMPTY_SPECIFICATION = "empty";
 
-    @NotNull
     private String interactionType;
-
-    @NotNull
     private Integer rank;
-
-    @NotNull
     private String specification;
 
-    @NotNull
     private Date dateCreated;
-    @NotNull
-    private Date lastUpdate;
+    private Date lastUpdated;
 
-    @NotNull
     private User owner;
-    @NotNull
-    private Sequence sequance;
-    @NotNull
-    private String state;
+    private Sequence sequence;
+    private String state = StateType.beforeStart.name();
 
-    private String result;
-    private String explanationRecommendationmapping;
+    private String results;
+    private String explanationRecommendationMapping;
 
-    public long getId() {
-        return id;
+    // TODO in list constraint
+    static constraints = {
+        interactionType inList: InteractionType.values()*.name()
+        state inList: StateType.values()*.name()
+        results nullable: true
+        explanationRecommendationMapping nullable: true
     }
 
-    public void setId(long id) {
-        this.id = id;
+//    static transients = ['interactionSpecification', 'interactionResultListService', 'responseRecommendationService', 'interactionService']
+
+    /**
+     * Get the result matrix as a list of float
+     * @return the result matrix as a list of float for each attempt
+     */
+    Map<String, List<Float>> resultsByAttempt() {
+        if (!results) {
+            return [:]
+        }
+        JsonSlurper jsonSlurper = new JsonSlurper()
+        jsonSlurper.parseText(results)
     }
 
-    public String getInteractionType() {
-        return interactionType;
+    boolean hasAnyResult() {
+        Map results = resultsByAttempt();
+
+        return results["1"]?.size() || results["2"]?.size()
     }
 
-    public void setInteractionType(String interactionType) {
-        this.interactionType = interactionType;
+    /**
+     * Get the results for the last attempt
+     * @return the results
+     */
+    List<Float> resultsOfLastAttempt() {
+        def resultsByAttempt = resultsByAttempt()
+        List<Float> res
+        if (resultsByAttempt["2"]) {
+            res = resultsByAttempt["2"]
+        } else if (resultsByAttempt["1"]) {
+            res = resultsByAttempt["1"]
+        } else {
+            res = []
+        }
+        res
     }
 
-    public Integer getRank() {
-        return rank;
+    InteractionService interactionService;
+
+    /**
+     * Process to perform after stop
+     */
+    void doAfterStop() {
+        state = StateType.afterStop.name()
+        if (isResponseSubmission()) {
+            doAfterStopOfResponseSubmission()
+        }
+        if (isEvaluation()) {
+            doAfterStopOfEvaluation()
+        }
     }
 
-    public void setRank(Integer rank) {
-        this.rank = rank;
+    private void doAfterStopOfEvaluation() {
+        def respSubmInter = sequence.responseSubmissionInteraction
+        if (sequence.statement.hasChoices()) {
+            respSubmInter.updateResults(2)
+            respSubmInter.save()
+        }
+        int attemptEvaluated = sequence.executionIsFaceToFace() ? 1 : 2
+        respSubmInter.findAllEvaluatedResponses(attemptEvaluated).each {
+            interactionService.updateMeanGradeOfResponse(it)
+        }
     }
 
-    public String getSpecification() {
-        return specification;
+    private void doAfterStopOfResponseSubmission() {
+        if (sequence.statement.hasChoices()) {
+            updateResults()
+        }
+        if (interactionSpecification.studentsProvideExplanation) {
+            updateExplanationRecommendationMapping()
+        }
+        save()
     }
 
-    public void setSpecification(String specification) {
-        this.specification = specification;
+    /**
+     * Find all evaluated responses for the current interaction
+     * @return the list of evaluated responses
+     */
+    public List<InteractionResponse> findAllEvaluatedResponses(int attempt = 1) {
+        InteractionResponse.findAllByInteractionAndAttempt(this, attempt)
     }
 
-    public Date getDateCreated() {
-        return dateCreated;
+    /**
+     * Get the interaction specification object
+     * @return the interaction specification
+     */
+    public JsonSpecification getInteractionSpecification() {
+        if (isResponseSubmission()) {
+            return new ResponseSubmissionSpecification(specification)
+        } else if (isEvaluation()) {
+            return new EvaluationSpecification(specification)
+        }
+        null
     }
 
-    public void setDateCreated(Date dateCreated) {
-        this.dateCreated = dateCreated;
+//    /**
+//     * Indicate if the interaction is a response submission
+//     * @return true if the interaction is a response submission
+//     */
+//    boolean isResponseSubmission() {
+//        return interactionType == InteractionType.ResponseSubmission.toString();
+//    }
+//
+//    /**
+//     * Indicate if the interaction is an evaluation
+//     * @return true if the interaction is an evaluation
+//     */
+//    boolean isEvaluation() {
+//        return sinteractionType == InteractionType.Evaluation.toString();
+//    }
+
+    /**
+     * Indicate if the interaction is read
+     * @return true if the interaction is read
+     */
+    boolean isRead() {
+        return interactionType == InteractionType.Read.toString();
     }
 
-    public Date getLastUpdate() {
-        return lastUpdate;
+    /**
+     * Calculate the number of choice interaction responses for the current interaction
+     * @return the number of responses
+     */
+    Integer interactionResponseCount(int attempt = 1) {
+        int res = InteractionResponse.countByInteractionAndAttempt(this, attempt);
+        return res;
     }
 
-    public void setLastUpdate(Date lastUpdate) {
-        this.lastUpdate = lastUpdate;
+    /**
+     * Count the number of evaluations
+     * @return the count
+     */
+    Integer evaluationCount() {
+        def count = PeerGrading.executeQuery(
+                '''
+        select count(distinct pg.grader) from PeerGrading pg
+        where pg.response in (from InteractionResponse resp where resp.interaction = ?)
+        ''',
+                [this])
+        count[0]
     }
 
-    public User getOwner() {
-        return owner;
+    /**
+     * Check if a user has already given a response for the current interaction
+     * @param user the user
+     * @return true if user has already given a response
+     */
+    boolean hasResponseForUser(User user, int attempt = 1) {
+        InteractionResponse.countByInteractionAndLearnerAndAttempt(this, user, attempt) > 0
+    }
+//
+//    /**
+//     * Get the response for the given user
+//     * @param user the user
+//     * @return the response
+//     */
+//    InteractionResponse responseForUser(User user, int attempt = 1) {
+//        return InteractionResponse.findByInteractionAndLearnerAndAttempt(this, user, attempt);
+//    }
+
+    /**
+     * Get the last response for the given user
+     * @param user the user
+     * @return the last response
+     */
+    InteractionResponse lastAttemptResponseForUser(User user) {
+        def res = InteractionResponse.findByInteractionAndLearnerAndAttempt(this, user, 2)
+        if (!res) {
+            res = InteractionResponse.findByInteractionAndLearnerAndAttempt(this, user, 1)
+        }
+        res
     }
 
-    public void setOwner(User owner) {
-        this.owner = owner;
+    /**
+     *
+     * @return the explanation recommendation map
+     */
+    Map<String, List<Long>> explanationRecommendationMap() {
+        if (!explanationRecommendationMapping) {
+            return [:]
+        }
+        JsonSlurper jsonSlurper = new JsonSlurper()
+        def res = jsonSlurper.parseText(explanationRecommendationMapping)
+        res
     }
 
-    public Sequence getSequance() {
-        return sequance;
-    }
+    /**
+     * return the state of the current interaction for the given learner
+     * @param user the learner
+     * @return the state of the current interaction for the given learner
+     */
+    String stateForLearner(User user) {
+        String state = this.state;
 
-    public void setSequance(Sequence sequance) {
-        this.sequance = sequance;
-    }
+        if(this.sequence.isStopped()) {
+            if(this.isRead() && sequence.resultsArePublished) {
+                return StateType.show.toString();
+            }
+            else if(this.rank <= this.sequence.activeInteraction.rank) {
+                return StateType.afterStop.toString();
+            }
+            else {
+                return StateType.beforeStart.toString();
+            }
+        }
 
-    public String getState() {
+        if (sequence.executionIsBlendedOrDistance() && !sequence.isStopped()) {
+            if (this == sequence.activeInteractionForLearner(user)) {
+
+                if(this.isRead() && this.sequence.resultsArePublished) {
+                    return StateType.show.toString();
+                }
+
+                if (sequence.executionIsBlended() && this.isRead()) {
+                    state = this.state;
+                } else {
+                    state = StateType.show.toString();
+                }
+            } else {
+                state = StateType.afterStop.toString();
+            }
+        }
         return state;
     }
 
-    public void setState(String state) {
-        this.state = state;
+    /**
+     * return the state of the current interaction for the given teacher
+     * @param user the teacher
+     * @return the state of the current interaction for the given teacher
+     */
+    String stateForTeacher(User user) {
+        String state = this.state
+        if (sequence.executionIsBlendedOrDistance()) {
+            state = StateType.afterStop.name()
+        }
+        if (sequence.executionIsBlended() && isRead()) {
+            state = this.state
+        }
+        state
     }
 
-    public String getResult() {
-        return result;
+    String stateForRegisteredUsers() {
+        if(this.isRead() && this.sequence.resultsArePublished) {
+            return StateType.show.name()
+        }
+
+        if(this.sequence.isStopped()) {
+            if(this.rank <= this.sequence.activeInteraction.rank) {
+                return StateType.afterStop
+            }
+            else {
+                return StateType.beforeStart
+            }
+        }
+
+        if (sequence.executionIsFaceToFace()) {
+            return this.state
+        } else if (isRead()) {
+            if (sequence.executionIsBlended()) {
+                return this.state
+            } else if (sequence.executionIsDistance()) {
+                return StateType.afterStop
+            }
+        } else { // ResponseSubmission or Evaluation for distance & blended
+            return StateType.show
+        }
     }
 
-    public void setResult(String result) {
-        this.result = result;
-    }
+    ResponseRecommendationService responseRecommendationService;
 
-    public String getExplanationRecommendationmapping() {
-        return explanationRecommendationmapping;
-    }
 
-    public void setExplanationRecommendationmapping(String explanationRecommendationmapping) {
-        this.explanationRecommendationmapping = explanationRecommendationmapping;
-    }
+//    private void updateExplanationRecommendationMapping(Integer attempt = 1) {
+//        def responses = InteractionResponse.findAllByInteractionAndAttempt(this, attempt)
+//        if (responses) {
+//            def mapping
+//            if (sequence.statement.hasChoices()) {
+//                mapping = responseRecommendationService.getRecommendedResponseIdByResponseId(responses)
+//            } else {
+//                int recommendationCount = sequence.evaluationSpecification.responseToEvaluateCount
+//                mapping = responseRecommendationService.getRecommendedResponseIdByResponseIdForOpenQuestion(responses, recommendationCount)
+//            }
+//            explanationRecommendationMapping = JsonOutput.toJson(mapping)
+//        }
+//    }
+
+    InteractionResultListService interactionResultListService;
+
+//    private void updateResults(Integer attempt = 1) {
+//        def responses = InteractionResponse.findAllByInteractionAndAttempt(this, attempt)
+//        if (responses) {
+//            def resList = interactionResultListService.buildResultListForInteractionAndResponses(this, responses)
+//            def resMap = resultsByAttempt() + [(attempt.toString()): resList]
+//            results = JsonOutput.toJson(resMap)
+//        }
+//    }
+
+
+}
+
+enum InteractionType {
+    ResponseSubmission,
+    Evaluation,
+    Read
 }
