e890096e9583631a76c99cc0dd6a7882c770508f
Gromnur <flavienregis@gmail.com>
2019-02-14 10:29:48 +0100

    Finish LearnerSequence class

--- src/main/java/org/elaastic/qtapi/entity/Interaction.java
+++ src/main/java/org/elaastic/qtapi/entity/Interaction.java
@@ -5,7 +5,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
-import java.util.Map;
 
 class Interaction {
 
@@ -35,23 +34,6 @@ class Interaction {
     private String results; // Can be null
     private String explanationRecommendationMapping; // Can be null
 
-//    static transients = ['interactionSpecification']
-
-
-    /**
-     * Get the interaction specification object
-     * @return the interaction specification
-     */
-    public JsonSpecification getInteractionSpecification() {
-        if (isResponseSubmission()) {
-            return new ResponseSubmissionSpecification(specification)
-        } else if (isEvaluation()) {
-            return new EvaluationSpecification(specification)
-        }
-        null
-    }
-
-
     /**
      * Indicate if the interaction is read
      * @return true if the interaction is read
@@ -68,13 +50,102 @@ class Interaction {
         if (!explanationRecommendationMapping) {
             return [:]
         }
-        JsonSlurper jsonSlurper = new JsonSlurper()
-        def res = jsonSlurper.parseText(explanationRecommendationMapping)
-        res
+        JsonSlurper jsonSlurper = new JsonSlurper();
+        def res = jsonSlurper.parseText(explanationRecommendationMapping);
+        return res;
     }
 
+    public static String getEmptySpecification() {
+        return EMPTY_SPECIFICATION;
+    }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getInteractionType() {
+        return interactionType;
+    }
+
+    public void setInteractionType(String interactionType) {
+        this.interactionType = interactionType;
+    }
+
+    public Integer getRank() {
+        return rank;
+    }
+
+    public void setRank(Integer rank) {
+        this.rank = rank;
+    }
+
+    public String getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(String specification) {
+        this.specification = specification;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Sequence getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(Sequence sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getResults() {
+        return results;
+    }
+
+    public void setResults(String results) {
+        this.results = results;
+    }
+
+    public String getExplanationRecommendationMapping() {
+        return explanationRecommendationMapping;
+    }
+
+    public void setExplanationRecommendationMapping(String explanationRecommendationMapping) {
+        this.explanationRecommendationMapping = explanationRecommendationMapping;
+    }
 }
 
 enum InteractionType {
